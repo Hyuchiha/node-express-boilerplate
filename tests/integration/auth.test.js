b8b33c72b8f9927f79584bbfc5c18b26007cd24f
hyuchiha <kev.pachec.lis@gmail.com>
2025-04-11 18:48:27 -0600

    Removed logs

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -269,12 +269,10 @@ describe('Auth routes', () => {
       const resetPasswordToken = tokenService.generateToken(userOne._id, expires, tokenTypes.RESET_PASSWORD);
       await tokenService.saveToken(resetPasswordToken, userOne._id, expires, tokenTypes.RESET_PASSWORD);
 
-      const rs = await request(app)
+      await request(app)
         .post('/v1/auth/reset-password')
         .query({ token: resetPasswordToken })
         .send({ password: 'password2' });
-
-      console.log(rs)
 
       const dbUser = await User.findById(userOne._id);
       const isPasswordMatch = await bcrypt.compare('password2', dbUser.password);
