c75943d1187d5fe405bd307ba30b1196d0dd1be6
hyuchiha <kev.pachec.lis@gmail.com>
2025-11-08 19:27:24 -0600

    Updated libs

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // sanitize request data
 app.use(xss());
-app.use(mongoSanitize());  // Enable when updated mongoSanitize
+//app.use(mongoSanitize());  // Enable when updated mongoSanitize
 
 // gzip compression
 app.use(compression());
